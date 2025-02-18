07b70bb3996b75bfe16fb8aee06bc6f5b45d725a
JadahH <Jhol1918@students.vsu.edu>
2025-02-17 20:20:48 -0500

    Implemented create and get methods

---
+++ shape/ShapeContainer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace shape;
+
+public class ShapeContainer
+{
+
+private List<Shape3D> shapes = new List<Shape3D>();
+
+    public void Create(Shape3D shape)
+    {
+        shapes.Add(shape);
+    }
+
+    public Shape3D Get(int index)
+    {
+        if (index >= 0 && index < shapes.Count)
+            return shapes[index];
+
+        throw new IndexOutOfRangeException("Invalid index.");
+    }
+
+
+}
